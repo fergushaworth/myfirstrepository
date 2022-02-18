eb8a64a4f863fb47bdf8421719036eb6f416b639
fergushaworth <fergushaworth@gmail.com>
2022-02-18 16:11:46 +0000

    Add files via upload

---
+++ PicUOAdv1.py
@@ -0,0 +1,90 @@
+print('''
+                         ...                             |     |    |   |
+    -`''-  _        ..########o...                       |     |    |   |
+ _.'     `: \     ,##8#########8##o,,     (      (       `|    |    |   |
+/ :  ___,-+-'   .#######8##8###8###8##,    )ailor )aturn  |    |    |   |
+`-+-'     ;   ,#8###########8#8########,,                 `|   `|   |   `|
+   `-...-'   :#88####8##8###########8###.,                 |    |  |'    |
+            .HXX8X88X#8XX########XX8X8XX#;,                `|   |  |     |
+           ,#O#X8888#88###########X####8X8,         _       `|  `\/'   ,/
+          :8##88##X######8#8######X######8#:      _| |__     `|      ,/
+         .######8##8####8)#)#(i#88####8####.:    |_   __|     `|    /
+       .,#88##8#88###H8I~-_  _-~8+8##8####8#,:     | |___    __.----._
+      :=#X##8#######,# ~-._()_.-~ ##########.,:    |  __ \   L__    __|
+    .:I8##88####88#:;_--__    __--_,:#88#####),:   | |  \ |     |--|
+   .:=8#XX#####88#LH =#O#\    /O##\i)##8######+:   | |  | \__  |'  `|
+ .':###X888###8#8#O) \##@/    \##=/#H8###8####.,:  |_|  `\___| `|--|'
+.':#####8####888L;;I,       |      #8###8##8###,.,              |  |
+:.88Li######8#888#,H.     .__,    :8##88#####8##o:             |'  `|
+:88X:##88##8##8#8##O8,           _8###########8#.8.           .|    |
+8X#+#888#8##8#8#H.###8#,_      _-'8##=#####88#XX,o:           | \__/  _
+X#..8#8##8##8888..8##8#8#-.__.-X#88##=8#####8#8#.,;           `.|__|-' |
+'   HH8###;##8##,+888#L#|__  __|##88##;8###8##X'               ||(__- |'
+_____                   |__()__|                _____          .|.:@#/_
+\    `---.________.-----'      `-----._____.---'    /          ||._--' |
+ \           oOOOOOOOOOOo__      ___oOOOOOOOOo    _/          | |(_----|
+  \_.--_        oOOOOOOOOo -   --   oOOOOOOo __.-'            | |(_--__)
+ _-:_-'           oOOOOOOOo        oOOOOOOo    `-_            | |.:(__ )
+<::<_      _        oOOOOOOo       oOOOOo |       -_          `||.:@#_/
+ ~-_:-__.-':'      xXXXxoOOOo ,|, oOOOoxXXXx _______\         |||.:@#|
+    -_|    |        xXXXXXXXXx\|/.xXXXXXXXx  \ \             / `|.:@#|
+      |   |'   _-.  xXXXXXxx--=O=--xxXXXXx|___| \           /  .|.:@#|
+      |   |__-':/    xXXxxXXx'/|\`.XXxxxXX:.  '  \        ./  .:|.:@#|
+      |     :::||    xxxXXXXx.'|`.xXXXXXxxx::.    \      /   .::|.:@#|
+      |     :::||_  xxXXXXXXXx/'\_xXXXXXXXXx::.    \    /   .::/|.:@#|
+      |    :::|' \`--xXXXXXXXx     xXXXXXx  \::.    \  /   .::/ |.:@#|
+      |    :::|   \   xXXXXXX      xXXXx     \::.   /\/   .::/  |.:@#|
+      |    ::|xx   \    xXx         xx        \::. /oO|  .::/   |.:@#|
+     |'   .::|XXXXxx|    `          '|   xxxxx \::/OoOO\.::/    |.:@#|
+     |    ::|XXXXXXX|                |xXXXXXx   \|oOOooOOOOo    |.:@#|
+ ,-__|__ .::|XXXXXX/__              |_XXXXXx      OoooOoooooo   |.:@#|
+ \ooooOOo::|XXXXXX/__ \,            | \XXXx       oOOOooOOOO    |.:@#|
+  \OOOoooooo|XxoOOOo.\. \          / ./oOOoXXXXXx   oOOOoooo    |.:@#|
+   |OOOOOOOO|oOOOOOOOo.\ \,       / /oOOOOOOoXXx       oOOo.    |.:@#|
+   |oooooooo|OOOOOOOOOOo.\ \     / /oOOOOOOOOOOOo [ -Tim Park ] |.:@#|
+
+''')
+
+print("Welcome to Saturn Adventurer!")
+print("Your mission here is to find the treasure.")
+
+print(" ")
+
+print("You aproach the swamp of Saturn.")
+print("there is a fork in the road aproaching.")
+choice1 = input("Which way would you like to turn? right or left?")
+choice1lwr = choice1.lower()
+
+if choice1lwr == "left":
+    print("You continue down the road safely")
+    print("Sudenly you come to the shors of a lake!")
+    choice2 = input("Do you Swim across the Lake OR wait until morning? swim OR wait?")
+    choice2lwr = choice2.lower()
+    if choice2lwr == "wait":
+        print("You set up camp by the shores of the lake and wait for morning.")
+        print("Mornig arives!")
+        print("A portal opens up by the tree next to your campp.")
+        print("You walk through it.")
+        print("You are now in a room with three doors")
+        choice3 = input("Which door do you chose to go through? red, yellow OR blue?")
+        choice3lwr = choice3.lower()
+        if choice3lwr == "red":
+            print("Inside the red door is a burning pit of fire!")
+            print("You fall in the pit and die!")
+
+        elif choice3lwr == "yellow":
+            print("The room is full of Gold. You Love Gold! You Win Life!")
+
+        elif choice3lwr == "blue":
+            print("The blue room is full of beasts you have never seen before!")
+            print("You go to pet one of the beasts!")
+            print("The beasts are not friendly and eat you. You Die!")
+        else:
+            print("You suddenly die of a heart attack!")
+
+    else:
+        print("Game Over - You get eaten by the moster of the lake!")
+else:
+    print("Game Over - the swamp troll owns this road and you cant pay the toll!")
+    
+
